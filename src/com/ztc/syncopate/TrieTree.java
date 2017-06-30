3f8e682503f737d51357ddceb8d118e10a21147f
yguo18 <yguoelect@hotmail.com>
2017-06-30 23:24:09 +0800

    add commit

--- src/com/ztc/syncopate/TrieTree.java
+++ src/com/ztc/syncopate/TrieTree.java
@@ -14,23 +14,100 @@ public class TrieTree {
 	 */
 	public void insert(String word){
 		//...
+		TrieNode node = root;
+		char[] words = word.toCharArray();
+		for(int i = 0; i < words.length; i++){
+			if(node.getChildrens().containsKey(words[i]+"")){
+				if(i == words.length-1){
+					TrieNode endNode = node.getChildrens().get(words[i]+"");
+					endNode.setFre(endNode.getFre()+1);
+					endNode.setEnd(true);
+				}
+			}else{
+				TrieNode newNode = new TrieNode(words[i]+"");
+				if(i == words.length -1){
+					newNode.setFre(1);
+					newNode.setEnd(true);
+					newNode.setRoot(false);
+				}
+				node.getChildrens().put(words[i]+"",newNode);
+			}
+			node = node.getChildrens().get(words[i]+"");
+		}
 	}
 	/**
 	 * ²éÕÒ²Ù×÷
 	 */
-	public int searchFre(){
+	public int searchFre(String word){
 		int fre = -1;
 		//....
+		TrieNode node = root;
+		char[] words = word.toCharArray();
+		for(int i = 0; i<words.length; i++){
+			if(node.getChildrens().containsKey(words[i]+"")){
+				node = node.getChildrens().get(words[i]+"");
+				fre = node.getFre();
+			}else{
+				fre = -1;
+				break;
+			}
+		}
 		return fre;
 	}
 	/**
-	 * ÇÐ·Ö²Ù×÷
+	 * ÊÇ·ñ°üº¬×Ö·û´®
+	 */
+	public boolean findInitialWith(String word){
+		TrieNode node = root;
+		char[] words = word.toCharArray();
+		for(int i =0; i<words.length; i++){
+			if(!node.getChildrens().containsKey(words[i]+"")){
+				return false;
+			}
+			node = node.getChildrens().get(words[i]+"");
+		}
+		if(node == null){
+			return false;
+		}
+		return true;
+	}
+	/**
+	 * ·Ö´Ê²Ù×÷
+	 * @param spell
+	 * @return
 	 */
 	public String splitSpell(String spell){
 		//....
-		
-		return spell;
+		TrieNode node = root;
+		char[] letters = spell.toCharArray();
+		String spells = "";
+		for(int i = 0; i<letters.length; i++){
+			if(node.getChildrens().containsKey(letters[i]+"")){
+				spells += letters[i];
+				node = node.getChildrens().get(letters[i]+"");
+			}else {
+				node = root;
+				spells += " ";
+				i--;
+			}
+		}
+		return spells;
 	}
-	
+	/**
+	 * ²âÊÔ
+	 * @param args
+	 */
+	public static void main(String[] args){
+		TrieTree trieTree = new TrieTree("guoyi");
+		trieTree.insert("ang");
+		trieTree.insert("wo");
+		trieTree.insert("my");
+		trieTree.insert("an");
+		trieTree.insert("y");
+		trieTree.insert("m");
+		System.out.println(trieTree.searchFre("an"));
+		System.out.println(trieTree.splitSpell("anym"));
+		System.out.println(trieTree.findInitialWith("an3"));
+	}
 	
 }
