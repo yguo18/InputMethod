e80b1bc3401f2c67441930161a189c7b7e71ba24
yguo18 <yguoelect@hotmail.com>
2017-07-13 17:22:52 +0800

    add commit

--- src/com/ztc/pinyinToChinese/Viterbi.java
+++ src/com/ztc/pinyinToChinese/Viterbi.java
@@ -6,26 +6,39 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 public class Viterbi {
+	/**
+	 * 
+	 * @param hmmParams   隐马尔科夫模型参数：start=初始概率，emission=发射概率， transition=状态转移概率
+	 * @param observations  观察序列
+	 * @param pathNum   记录的路径个数
+	 * @param isLog  是否使用log函数求最大值
+	 * @return
+	 */
 	public static ArrayList<PriorityQueueWrapper.Item> compute(HmmParamsInterface hmmParams, ArrayList<String> observations, int pathNum, boolean isLog) {
         double minProb = 3.14e-200;
         ArrayList<HashMap<String, PriorityQueueWrapper>> V = new ArrayList<HashMap<String, PriorityQueueWrapper>>();
         int t = 0;
-        String curObs = observations.get(0);
+        String curObs = observations.get(0);//获取首个拼音
 
         ArrayList<String> prevStates, curStates;
-        prevStates = curStates = hmmParams.getStates(curObs);
+        prevStates = curStates = hmmParams.getStates(curObs); // 拼音对应的汉字用链表来表示
+//        prevStates = curStates = curObs;
+        
 
-        Double score = 0.0;
-        ArrayList<String> path = null;
+        Double score = 0.0;   // 打分
+        ArrayList<String> path = null;  // 记录路径
         V.add(new HashMap<String, PriorityQueueWrapper>());
         for (String state : curStates) {
             if (isLog) {
                 score = Math.log(Math.max(hmmParams.start(state), minProb)) + Math.log(Math.max(hmmParams.emission(state, curObs), minProb));
             } else {
+            	/**
+            	 * hmmParams.start(state):返回当前汉字的概率，hmmParams.emission(state, curObs)：返回由拼音转换成当前汉字的发射概率
+            	 */
                 score = Math.max(hmmParams.start(state), minProb) * Math.max(hmmParams.emission(state, curObs), minProb);
             }
             path = new ArrayList<>();
-            path.add(state);
+            path.add(state);//把汉字加入列表
 
             if (!V.get(0).containsKey(state)) {
                 V.get(0).put(state, new PriorityQueueWrapper(pathNum));
@@ -72,6 +85,7 @@ public class Viterbi {
 
         PriorityQueueWrapper priorityQueueWrapperResult = new PriorityQueueWrapper(pathNum);
         if (observations.size() > 1) {
+        	
             t = 1;
         } else {
             t = 0;
