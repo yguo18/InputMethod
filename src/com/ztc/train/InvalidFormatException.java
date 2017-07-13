35c6389ca93cdd5b28a8d3f295074f01a052df84
yguo18 <yguoelect@hotmail.com>
2017-07-13 21:42:09 +0800

    add Exception class

---
+++ src/com/ztc/train/InvalidFormatException.java
@@ -0,0 +1,11 @@
+package com.ztc.train;
+/**
+ * 定义异常类
+ * @author yguo
+ *
+ */
+public class InvalidFormatException extends Exception{
+	public InvalidFormatException(String message){
+		super(message);
+	}
+}
---
+++ src/com/ztc/train/PinyinException.java
@@ -0,0 +1,9 @@
+package com.ztc.train;
+
+public class PinyinException extends Exception{
+//	private static final long serialVersionUID = 1L;
+
+    public PinyinException(String message) {
+        super(message);
+    }
+}
